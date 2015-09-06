ec015c2505a008354263796f1f894c892e52687d
Michiel Scholten <michiel@diginaut.net>
2015-09-06 13:36:13 +0200

    Introduced click for cli options

--- paragoo.py
+++ paragoo.py
@@ -2,37 +2,71 @@ import os
 from jinja2 import Template
 import jinja2
 import yaml
+import click
 import settings
 
-# Templates can live anywhere, define them in settings.py
-f = open(os.path.join(setting.project_path, 'site.yaml'))
+## Main program
+@click.group()
+def cli():
+    """
+    Paragoo website generator
+    """
+    pass
 
-print('Reading structure from ' + os.path.join(setting.project_path, 'site.yaml'))
 
-structure = yaml.safe_load(f)
-f.close()
+@cli.command()
+@click.option('-c', '--config', prompt='Site config path')
+def check_config(config):
+    """
+    Check site config (site.yaml) for correctness
+    """
+    click.secho('Needs implementing', fg='red')
 
-print structure
 
-# Templates can live anywhere, define them in settings.py
-template_dir = settings.template_path
-#loader = jinja2.FileSystemLoader(template_dir)
-loader = jinja2.FileSystemLoader(
-        [template_dir,
-         os.path.join(os.path.dirname(__file__), 'templates/includes'),
-         os.path.join(os.path.dirname(__file__), 'templates')])
-environment = jinja2.Environment(loader=loader)
+#@cli.command('run_disruptions')
+@cli.command()
+@click.option('-c', '--config', prompt='Site config path')
+def generate_site(config):
+    """
+    Generate the website specified in the config
+    """
 
-template = environment.get_template('base.html')
+    # Templates can live anywhere, define them in settings.py
+    f = open(os.path.join(setting.project_path, 'site.yaml'))
 
-for section in structure:
-    # loop over the sections
-    print section
-    section_data = structure[section]
-    print(section_data['name'])
-    for page in section_data:
-        # loop over its pages
-        print(page)
+    print('Reading structure from ' + os.path.join(setting.project_path, 'site.yaml'))
 
-#template = Template('Hello {{ name }}!')
-template.render(name='John Doe')
+    structure = yaml.safe_load(f)
+    f.close()
+
+    print structure
+
+    # Templates can live anywhere, define them in settings.py
+    template_dir = settings.template_path
+    #loader = jinja2.FileSystemLoader(template_dir)
+    loader = jinja2.FileSystemLoader(
+            [template_dir,
+             os.path.join(os.path.dirname(__file__), 'templates/includes'),
+             os.path.join(os.path.dirname(__file__), 'templates')])
+    environment = jinja2.Environment(loader=loader)
+
+    template = environment.get_template('base.html')
+
+    for section in structure:
+        # loop over the sections
+        print section
+        section_data = structure[section]
+        print(section_data['name'])
+        for page in section_data:
+            # loop over its pages
+            print(page)
+
+    #template = Template('Hello {{ name }}!')
+    template.render(name='John Doe')
+
+
+if __name__ == '__main__':
+    """
+    Paragoo is ran standalone
+    """
+    cli()
