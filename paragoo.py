fac1a5d4d6a7805079fef5777ae80bfca0c8df7c
Michiel Scholten <michiel@diginaut.net>
2015-09-09 13:12:45 +0200

    Support flat source file structure

--- paragoo.py
+++ paragoo.py
@@ -20,24 +20,44 @@ def ensure_dir(f):
         os.makedirs(d)
 
 
-def load_page_source(section_dir, page, page_data):
-    filename = os.path.join(section_dir, page)
-    try:
-        content_type = page_data['type']
-    except KeyError:
-        content_type = 'markdown'
-    try:
-        filename = page_data['file']
-    except KeyError:
-        print 'wtf'
-    filename += '.' + CONTENT_TYPES[content_type]
-    print filename
+def get_file_contents(filename):
     data = None
     try:
         with open(filename) as pf:
             data = pf.read()
     except IOError:
-        print 'File not found: ' + filename
+        #print 'File not found: ' + filename
+        pass
+    return data
+
+
+def load_page_source(source_uses_subdirs, section_dir, page, page_data):
+    if source_uses_subdirs:
+        filename = os.path.join(section_dir, page)
+    else:
+        filename = section_dir + '_' + page
+
+    try:
+        content_type = page_data['type']
+    except KeyError:
+        content_type = 'unknown'
+    try:
+        filename = page_data['file']
+    except KeyError:
+        # No filename was defined explicitely (normal behaviour)
+        pass
+    if content_type == 'unknown':
+        # Try to find the file
+        for ct in CONTENT_TYPES:
+            try_filename = filename + '.' + ct
+            data = get_file_contents(try_filename)
+            if data:
+                print try_filename
+                break
+    else:
+        filename += '.' + CONTENT_TYPES[content_type]
+        print filename
+        data = get_file_contents(try_filename)
     if data and content_type == 'markdown':
         data = markdown.markdown(data, output_format='html5')
     return data
@@ -63,10 +83,10 @@ def check_config(site):
 
 #@cli.command('run_disruptions')
 @cli.command()
-@click.option('-s', '--site', prompt='Site path')
-@click.option('-t', '--template', prompt='Template path')
-@click.option('-o', '--output_dir', prompt='Output path for generated content')
-@click.option('--clean/--noclean', default=False)
+@click.option('-s', '--site', help='Site path', prompt='Site path')
+@click.option('-t', '--template', help='Template path', prompt='Template path')
+@click.option('-o', '--output_dir', help='Output path for generated content', prompt='Output path for generated content')
+@click.option('--clean/--noclean', help='Clean the output_dir first or not', default=False)
 def generate_site(site, template, output_dir, clean):
     """
     Generate the website specified in the config
@@ -116,32 +136,43 @@ def generate_site(site, template, output_dir, clean):
     for field in site_fields:
         site_data[field] = structure[field]
 
+    source_uses_subdirs = True
+    try:
+        source_uses_subdirs = structure['subdirs']
+    except KeyError:
+        print 'Defaulting to searching sub directories for source files'
+
     for section in structure['sections']:
         # Iterate over the sections
         section_data = structure['sections'][section]
         section_filename = os.path.join(output_dir, section)
         source_section_filename = os.path.join(site, section)
+        if not source_uses_subdirs:
+            source_section_filename = os.path.join(site, 'pages', section)
         #print section_filename
-        for page in section_data['pages']:
-            # Loop over its pages
-            page_data = structure['sections'][section]['pages'][page]
-            htmlbody = load_page_source(source_section_filename, page, page_data)
-            data = site_data.copy()
-            data['site'] = site_data
-            try:
-                data['author'] = page_data['author']
-            except KeyError:
-                data['author'] = site_data['author']
-            data['page'] = page_data
-            data['htmlbody'] = htmlbody
-            data['structure'] = structure
-            output = template.render(data)
-            filename = os.path.join(section_filename, page + '/index.html')
-            print filename
-            ensure_dir(filename)
-            pf = open(filename, 'w')
-            pf.write(output)
-            pf.close()
+        if not 'pages' in section_data:
+            print '- section ' + section + ' does not have pages'
+        else:
+            for page in section_data['pages']:
+                # Loop over its pages
+                page_data = structure['sections'][section]['pages'][page]
+                htmlbody = load_page_source(source_uses_subdirs, source_section_filename, page, page_data)
+                data = site_data.copy()
+                data['site'] = site_data
+                try:
+                    data['author'] = page_data['author']
+                except KeyError:
+                    data['author'] = site_data['author']
+                data['page'] = page_data
+                data['htmlbody'] = htmlbody
+                data['structure'] = structure
+                output = template.render(data)
+                filename = os.path.join(section_filename, page + '/index.html')
+                print filename
+                ensure_dir(filename)
+                pf = open(filename, 'w')
+                pf.write(output)
+                pf.close()
     print 'done'
 
 
