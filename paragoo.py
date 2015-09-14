286703b3b774b13a682cdb4a6171554be9e0284e
Michiel Scholten <michiel@diginaut.net>
2015-09-14 14:01:34 +0200

    Support for ordered loading of a yaml file

--- paragoo.py
+++ paragoo.py
@@ -6,6 +6,19 @@ import click
 import markdown
 import shutil
 import datetime
+from collections import OrderedDict
+
+
+def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    class OrderedLoader(Loader):
+        pass
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load(stream, OrderedLoader)
 
 
 CONTENT_TYPES = {
@@ -121,7 +134,7 @@ def generate_site(site, template, output_dir, clean):
 
         print(' r  Reading structure from ' + os.path.join(site, 'site.yaml'))
 
-        structure = yaml.safe_load(f)
+        structure = ordered_load(f, yaml.SafeLoader)
         f.close()
     except IOError as e:
         print e
