b2e700831f75f35aab0c58755fd2a2601cb855b5
Michiel Scholten <michiel@diginaut.net>
2015-09-30 21:11:37 +0200

    Slice and dice the news items and render with template

--- plugins/news.py
+++ plugins/news.py
@@ -18,12 +18,28 @@ def render(site_path, environment, params):
         content = ''
         with open(filename, 'r') as f:
             content = f.readlines()
+        first = 1
+        last = len(content)
+        if len(params) == 2:
+            first = int(params[1])
+        if len(params) == 3:
+            first = int(params[1])
+            last = int(params[2])
         template = environment.get_template('newsitem.html')
-        print content
         items = []
+        counter = 1
         for item in content:
-            parts = item.split('=')
-            items.append({'key': parts[0], 'content': parts[1]})
+            if item.strip() and counter >= first and counter <= last:
+                parts = item.split('=')
+                key = parts[0]
+                del parts[0]
+                content = '='.join(parts) # content may/will contain ='s
+                content = content.replace('\\n', '\n')
+                items.append({'key': key.strip(), 'content': content.strip()})
+                counter += 1
+            elif item.strip():
+                # Only count non-empty lines
+                counter += 1
         data = {'items': items}
         return template.render(data)
     else:
